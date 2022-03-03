218f7a0beb2f9179f8d4dd81384ea995ef3f2225
Siva Priya <Siva Priya@DESKTOP-VKENIE7>
2022-03-03 18:03:34 +0530

    Done some changes

---
+++ hello-world-example/src/main/java/com/tss/example/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.tss.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter writer = response.getWriter();
+		writer.print("<h1>Hello world</h1>");
+		writer.print("Welcome to this world");
+	}
+
+}
